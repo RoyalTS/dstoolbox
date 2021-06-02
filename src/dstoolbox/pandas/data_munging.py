54361c78cdf1adc9e6d68214bee3990cb494e390
Tobias Schmidt <tobias.schmidt@deliveroo.co.uk>
2021-06-02 09:55:14 +0100

    utility functions for pandas data munging sanity checks

--- src/dstoolbox/pandas/data_munging.py
+++ src/dstoolbox/pandas/data_munging.py
@@ -4,6 +4,7 @@ from typing import List
 
 import pandas as pd
 from loguru import logger
+from itertools import tee
 
 
 def mixed_domain(series: pd.Series) -> pd.Series:
@@ -230,3 +231,37 @@ def decimal_time_of_day(ser: pd.Series) -> pd.Series:
         float series
     """
     return (ser.dt.hour * 3600 + ser.dt.minute * 60 + ser.dt.second) / 3600
+
+
+
+from collections.abc import Iterable
+
+def _pairwise(iterable: Iterable) -> list:
+    """Returns all pairs of successive elements of a list:
+
+    s -> (s0,s1), (s1,s2), (s2, s3), ...
+    """
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+def check_lengths_all_equal(*args):
+    """Check the lengths of all passed objects is the same."""
+    lengths = [len(x) for x in args]
+    if not len(set(lengths)) == 1:
+        raise ValueError(
+            f"Passed Series and DataFrames don't all have the same lenghts: {lengths}"
+        )
+    else:
+        return lengths[0]
+
+
+def check_indices_all_equal(*args):
+    """Check the indices of all passed pandas Series or DataFrames are equal."""
+    pairs = _pairwise(args)
+    pairs_equal = [x.index.equals(y.index) for x, y in pairs]
+    if not all(pairs_equal):
+        unequal_pairs = [pair for ix, pair in enumerate(pairs) if not pairs_equal[ix]]
+        raise ValueError(
+            f"Passed Series and DataFrames don't all have indentical indices. :{unequal_pairs[0]}"
+        )
