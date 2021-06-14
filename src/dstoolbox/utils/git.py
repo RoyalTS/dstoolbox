f511d5e5b1566072125b93a01c1e8ef72416e254
Tobias Schmidt <tobias.schmidt@deliveroo.co.uk>
2021-06-14 22:41:17 +0100

    helper for getting the latest git tag

---
+++ src/dstoolbox/utils/git.py
@@ -0,0 +1,13 @@
+import subprocess
+
+
+def git_tag() -> str:
+    """Get the latest tag of the active git repo.
+    Fall back to the latest commit hash if no tag exists
+
+    Returns
+    -------
+    str
+        git tag or commit hash
+    """
+    return subprocess.check_output(["git", "describe", "--always"]).strip()
