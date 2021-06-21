17d6128b5878312f13e698919b85fe5e4dce95aa
Tobias Schmidt <tobias.schmidt@deliveroo.co.uk>
2021-06-21 18:14:43 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="dstoolbox",
     packages=find_packages(),
-    version="0.1.7",
+    version="0.1.8",
     description="A collection of utility functions for data science applications.",
     author="Tobias Schmidt",
     license="MIT",
