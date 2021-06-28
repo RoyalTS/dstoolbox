dfc0f6ac1a64a8aae8808d1cf70b71e5bc1ca3fb
Tobias Schmidt <royalts@gmail.com>
2021-06-28 16:36:00 +0100

    allow empty passwords, black

--- src/dstoolbox/snowflake/connections.py
+++ src/dstoolbox/snowflake/connections.py
@@ -1,13 +1,15 @@
 import sqlalchemy as sa
 from snowflake.sqlalchemy import URL
 import snowflake.connector
-from environs import Env
+import environs
 
 from loguru import logger
 from pprint import pformat
 
 
-def get_snowflake_credentials_from_env(prefix :str="SNOWFLAKE_", mode:str='sqlalchemy') -> dict:
+def get_snowflake_credentials_from_env(
+    prefix: str = "SNOWFLAKE_", mode: str = "sqlalchemy"
+) -> dict:
     """Get snowflake credentials from environment variables
 
     Parameters
@@ -23,36 +25,41 @@ def get_snowflake_credentials_from_env(prefix :str="SNOWFLAKE_", mode:str='sqlal
         stripped of the prefix
     """
 
-    env = Env()
+    env = environs.Env()
     env.read_env()
 
     creds = {}
 
     with env.prefixed(prefix=prefix):
-        creds['host'] = env("HOST", "deliveroo.eu-central-1")
-        creds['username'] = env("USERNAME")
-        creds['password'] = env('PASSWORD', '')
-        creds['warehouse'] = env("WAREHOUSE")
-        creds['role'] = env("ROLE")
-        creds['database'] = env("DATABASE")
-        creds['schema'] = env("SCHEMA")
+        creds["host"] = env("HOST", "deliveroo.eu-central-1")
+        creds["username"] = env("USERNAME")
+        try:
+            creds["password"] = env("PASSWORD")
+        except environs.EnvError:
+            pass
+        creds["warehouse"] = env("WAREHOUSE")
+        creds["role"] = env("ROLE")
+        creds["database"] = env("DATABASE")
+        creds["schema"] = env("SCHEMA")
 
     # SSO if no password supplied
-    if creds['password'] == '':
-        creds['authenticator']="externalbrowser"
+    if "password" not in creds:
+        creds["authenticator"] = "externalbrowser"
 
     # these are the argument names sqlalchemy uses
-    if mode == 'sqlalchemy':
-        if 'host' in creds and (not 'account' in creds):
-            creds['account'] = creds.pop('host')
-        if 'username' in creds and (not 'user' in creds):
-            creds['user'] = creds.pop('username')
+    if mode == "sqlalchemy":
+        if "host" in creds and (not "account" in creds):
+            creds["account"] = creds.pop("host")
+        if "username" in creds and (not "user" in creds):
+            creds["user"] = creds.pop("username")
 
     # log sanitize credentials
     clean_creds = creds.copy()
-    if 'password' in clean_creds:
-        clean_creds['password'] = '*****'
-    logger.debug(f"Read in Snowflake credentials from the entironment:\n{pformat(clean_creds)}")
+    if "password" in clean_creds:
+        clean_creds["password"] = "*****"
+    logger.debug(
+        f"Read in Snowflake credentials from the entironment:\n{pformat(clean_creds)}"
+    )
 
     return creds
 
@@ -68,8 +75,8 @@ def create_snowflake_connection(creds: dict):
         'warehouse', 'role', 'schema', 'database' and optionally 'password'. If
         no password is supplied authentication via SSO is triggered
     """
-    if not 'password' in creds:
-        creds['authenticator']="externalbrowser"
+    if not "password" in creds:
+        creds["authenticator"] = "externalbrowser"
 
     ctx = snowflake.connector.connect(**creds)
 
@@ -92,8 +99,8 @@ def create_snowflake_engine(creds: dict) -> sa.engine.base.Engine:
     sa.engine.base.Engine
         SQLAlchemy engine
     """
-    if not 'password' in creds:
-        creds['authenticator']="externalbrowser"
+    if not "password" in creds:
+        creds["authenticator"] = "externalbrowser"
 
     engine = sa.create_engine(URL(**creds))
 
