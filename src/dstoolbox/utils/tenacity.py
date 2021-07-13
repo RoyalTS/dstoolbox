832bec2021eac1226f0a80c028f6ab465d9f7189
Tobias Schmidt <royalts@gmail.com>
2021-07-13 13:42:53 +0100

    update before_sleep_loguru to keep up with upstream

--- src/dstoolbox/utils/tenacity.py
+++ src/dstoolbox/utils/tenacity.py
@@ -1,7 +1,6 @@
 import typing
 
-from tenacity import RetryCallState, _utils
-from tenacity.compat import get_exc_info_from_future
+from tenacity import _utils
 
 if typing.TYPE_CHECKING:
     import loguru
@@ -17,26 +16,23 @@ def before_sleep_loguru(
 
     def log_it(retry_state: "RetryCallState") -> None:
         if retry_state.outcome.failed:
-            exception = retry_state.outcome.exception()
-            verb, value = "raised", f"{type(exception).__name__}: {exception}"
+            ex = retry_state.outcome.exception()
+            verb, value = "raised", f"{ex.__class__.__name__}: {ex}"
 
             if exc_info:
-                local_exc_info = get_exc_info_from_future(retry_state.outcome)
+                local_exc_info = retry_state.outcome.exception()
             else:
                 local_exc_info = False
         else:
             verb, value = "returned", retry_state.outcome.result()
             local_exc_info = False  # exc_info does not apply when no exception
 
-        callback_name = _utils.get_callback_name(retry_state.fn)
-        # FIXME: currently fails
-        # sleep_time = getattr(retry_state.next_action, "sleep")
         nth_attempt = _utils.to_ordinal(retry_state.attempt_number)
         logger.opt(exception=local_exc_info).log(
             log_level,
-            # f"Retrying {callback_name} in {sleep_time:.2f} seconds as its"
-            f"Retrying {callback_name} soonish as its"
-            f" {nth_attempt} attempt {verb} {value}.",
+            f"Retrying {_utils.get_callback_name(retry_state.fn)} "
+            f"for the {nth_attempt} time "
+            f"in {retry_state.next_action.sleep} seconds as it {verb} {value}.",
         )
 
     return log_it
