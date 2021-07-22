2245f39a68d3e3a1241a6e6404d87b99f2b53d12
Tobias Schmidt <royalts@gmail.com>
2021-07-22 13:21:09 +0100

    bugfixes

--- src/dstoolbox/__init__.py
+++ src/dstoolbox/__init__.py
@@ -1,3 +1,6 @@
-from . import _version
+from ._version import get_versions
 
-__version__ = _version.get_versions()["version"]
+v = get_versions()
+__version__ = v.get("closest-tag", v["version"])
+__git_version__ = v.get("full-revisionid")
+del get_versions, v
