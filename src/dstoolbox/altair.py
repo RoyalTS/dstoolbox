015bd9b8aa211fe80df024b6092a7b526d8c052c
Tobias Schmidt <royalts@gmail.com>
2021-07-28 15:41:27 +0100

    date_axis convenience function for altair

--- src/dstoolbox/altair.py
+++ src/dstoolbox/altair.py
@@ -1,3 +1,4 @@
+import altair as alt
 from loguru import logger
 
 
@@ -8,3 +9,35 @@ def sample_for_altair(df):
         return df.sample(5000)
     else:
         return df
+
+
+def date_axis(date_var: str, interval: str = "month") -> alt.Axis:
+    """Generate a date axis for Altair plots.
+
+    Parameters
+    ----------
+    date_var : str
+        variable name for the date variable
+    interval : str, optional
+        the tick interval for the date axis. Default is "month"
+
+    Returns
+    -------
+    alt.Axis
+    """
+    # calendar week formatting
+    if interval == "week":
+        return alt.X(
+            date_var,
+            title=None,
+            axis=alt.Axis(format="W%W"),
+            scale=alt.Scale(nice={"interval": "day", "step": 7}),
+        )
+    # day/month formatting
+    elif interval == "month":
+        return alt.X(
+            date_var,
+            title=None,
+            axis=alt.Axis(format="%e. %b"),
+            scale=alt.Scale(nice={"interval": "month", "step": 1}),
+        )
